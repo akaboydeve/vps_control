b13297267eea86afb4c056b81aefaaf04867dbba
Akash Sardar <73843791+akaboydeve@users.noreply.github.com>
2025-07-24 23:40:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ load_dotenv()
 
 app = FastAPI()
 
+origins = [
+    "https://vps-control-web.vercel.app",
+    "https://vps-control-web.hexonode.com",
+    ""http://localhost:3000"",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
